90f6c507cf46b746459ffa6f3134e7cfb5079c97
elic121 <elic121@ajou.ac.kr>
2023-04-21 21:57:06 +0900

    <update> design
    [23-04-21 21:57]

--- lib/handler/handler_signin.dart
+++ lib/handler/handler_signin.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
@@ -44,10 +45,12 @@ Scaffold isLoadingScreen(){
     body: Center(
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+        children: const[
           SizedBox(height: 24.0),
-          CircularProgressIndicator(
-            valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+          CupertinoActivityIndicator(
+            animating: true,
+            color: Colors.white,
+            radius: 35,
           ),
           SizedBox(height: 16.0),
           Text(
