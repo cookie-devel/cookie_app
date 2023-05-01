a87ec8f16956467b0c09ff928f86e4b4c59a9f7f
elic121 <elic121@ajou.ac.kr>
2023-05-02 02:07:27 +0900

    <update> minor change
    [23-05-02 02:07]

--- lib/pages/tabs/settings/settings.tab.dart
+++ lib/pages/tabs/settings/settings.tab.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:cookie_app/handler/storage.dart';
 import 'settings.appbar.dart';
 import 'detail/MyProfile.page.dart';
+import 'package:cookie_app/components/NavigatePage.dart';
 
 class SettingsWidget extends StatefulWidget {
   const SettingsWidget({Key? key}) : super(key: key);
@@ -17,33 +18,16 @@ class _SettingsWidgetState extends State<SettingsWidget> {
     return Scaffold(
       appBar: settingsAppbar(context),
       body: ListView(
+        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
         children: <Widget>[
           ListTile(
             title: const Text('프로필 관리'),
             trailing: const Icon(Icons.arrow_forward_ios),
             onTap: () {
-              Navigator.of(context).push(
-                PageRouteBuilder(
-                  transitionDuration: const Duration(milliseconds: 150),
-                  reverseTransitionDuration: const Duration(milliseconds: 150),
-                  pageBuilder: (context, animation, secondaryAnimation) {
-                    return const MyProfileWidget();
-                  },
-                  transitionsBuilder:
-                      (context, animation, secondaryAnimation, child) {
-                    return SlideTransition(
-                      position: Tween(
-                        begin: const Offset(1.0, 0.0),
-                        end: Offset.zero,
-                      ).animate(animation),
-                      child: child,
-                    );
-                  },
-                ),
-              );
+              navigateSlide(context, MyProfileWidget());
             },
           ),
-          const Divider(height: 1),
+          const Divider(),
           ListTile(
             title: const Text('알림'),
             trailing: const Icon(Icons.arrow_forward_ios),
@@ -51,7 +35,7 @@ class _SettingsWidgetState extends State<SettingsWidget> {
               // Handle notification settings onTap
             },
           ),
-          const Divider(height: 1),
+          const Divider(),
           ListTile(
             title: const Text('친구'),
             trailing: const Icon(Icons.arrow_forward_ios),
@@ -59,7 +43,7 @@ class _SettingsWidgetState extends State<SettingsWidget> {
               // Handle privacy settings onTap
             },
           ),
-          const Divider(height: 1),
+          const Divider(),
           ListTile(
             title: const Text('언어'),
             trailing: const Icon(Icons.arrow_forward_ios),
@@ -67,7 +51,7 @@ class _SettingsWidgetState extends State<SettingsWidget> {
               // Handle language settings onTap
             },
           ),
-          const Divider(height: 1),
+          const Divider(),
           ListTile(
             title: const Text('기타'),
             trailing: const Icon(Icons.arrow_forward_ios),
@@ -75,7 +59,7 @@ class _SettingsWidgetState extends State<SettingsWidget> {
               // Handle about onTap
             },
           ),
-          const Divider(height: 1),
+          const Divider(),
           ListTile(
             title: const Text(
               '로그아웃',
@@ -89,7 +73,7 @@ class _SettingsWidgetState extends State<SettingsWidget> {
               _showAlertDialog(context);
             },
           ),
-          const Divider(height: 1),
+          const Divider(),
         ],
       ),
     );
@@ -103,7 +87,7 @@ Future<void> _logout(BuildContext context) async {
   const destination = SignInWidget();
 
   // 현재 페이지를 스택에서 제거하고 대상 페이지를 스택에 추가
-  Navigator.pushReplacement(
+  await Navigator.pushReplacement(
     context,
     MaterialPageRoute(builder: (BuildContext context) => destination),
   );
@@ -117,18 +101,18 @@ Future<void> _showAlertDialog(BuildContext context) async {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(16.0),
         ),
-        title: Text(
+        title: const Text(
           '로그아웃',
           style: TextStyle(
             fontWeight: FontWeight.bold,
           ),
         ),
-        content: Text(
+        content: const Text(
           '정말로 로그아웃 하시겠습니까?',
         ),
         actions: <Widget>[
           TextButton(
-            child: Text(
+            child: const Text(
               '아니오',
               style: TextStyle(
                 color: Colors.grey,
@@ -151,7 +135,7 @@ Future<void> _showAlertDialog(BuildContext context) async {
               Navigator.of(context).pop();
             },
             style: ElevatedButton.styleFrom(
-              primary: Colors.blue,
+              backgroundColor: Colors.blue,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
               ),
