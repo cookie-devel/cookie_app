439298b02393f97100313cbc6548714ba9d1ba1d
parkjbdev <dev@parkjb.com>
2023-05-05 04:05:41 +0900

    load friends from server response

--- lib/pages/tabs/friends/friends.tab.dart
+++ lib/pages/tabs/friends/friends.tab.dart
@@ -4,6 +4,7 @@ import 'package:cookie_app/components/friends/FriendProfileWidget.dart';
 import 'package:cookie_app/schema/FriendInfo.dart';
 import 'package:flutter/services.dart';
 import 'package:cookie_app/pages/tabs/friends/friends.appbar.dart';
+import 'package:cookie_app/handler/data.dart';
 
 class FriendsGrid extends StatefulWidget {
   const FriendsGrid({Key? key}) : super(key: key);
@@ -18,15 +19,12 @@ class _FriendsGridState extends State<FriendsGrid> {
   @override
   void initState() {
     super.initState();
-    getSampleData();
+    getData();
   }
 
-  void getSampleData() async {
-    final String res = await rootBundle.loadString('assets/data/friends.json');
-    final data = await json.decode(res);
-
+  void getData() {
     setState(() {
-      profiles = data;
+      profiles = account['friendList'];
     });
   }
 
@@ -36,22 +34,32 @@ class _FriendsGridState extends State<FriendsGrid> {
       appBar: friendsAppbar(context),
       body: Padding(
         padding: const EdgeInsets.fromLTRB(10, 20, 10, 8),
-        child: GridView.builder(
-          itemCount: profiles.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-            crossAxisCount: 3,
-            childAspectRatio: 0.8,
-            mainAxisSpacing: 15.0,
-            crossAxisSpacing: 10.0,
-          ),
-          itemBuilder: (BuildContext context, int index) {
-            final Map<String, dynamic> profile = profiles[index];
+        child: profiles.isNotEmpty
+            ? GridView.builder(
+                itemCount: profiles.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 3,
+                  childAspectRatio: 0.8,
+                  mainAxisSpacing: 15.0,
+                  crossAxisSpacing: 10.0,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  final Map<String, dynamic> profile = profiles[index];
 
-            return FriendProfileWidget(
-              user: returnUserInfo(profile),
-            );
-          },
-        ),
+                  return FriendProfileWidget(
+                    user: returnUserInfo(profile),
+                  );
+                },
+              )
+            : const Center(
+                child: Text(
+                  '친구를 추가해보세요!',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
       ),
     );
   }
