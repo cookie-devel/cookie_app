7abb80ae3279235860779609215cdfcf330f79c1
parkjbdev <dev@parkjb.com>
2023-05-05 23:58:22 +0900

    add: validator with msg

--- lib/handler/account.validator.dart
+++ lib/handler/account.validator.dart
@@ -9,6 +9,20 @@ bool isValidID(id) {
       IDPW_REGEX.hasMatch(id);
 }
 
+String? validateID(id) {
+  if (id == null || id.isEmpty) {
+    return '아이디를 입력해주세요.';
+  } else if (id.length < MIN_ID_LENGTH) {
+    return '최소 $MIN_ID_LENGTH자 이상 입력해주세요.';
+  } else if (id.length > MAX_ID_LENGTH) {
+    return '최대 $MAX_ID_LENGTH자 이하로 입력해주세요.';
+  } else if (!IDPW_REGEX.hasMatch(id)) {
+    return '영문, 숫자, 특수문자(!@#\$&*~-)만 입력해주세요.';
+  } else {
+    return null;
+  }
+}
+
 const MIN_PW_LENGTH = 10;
 const MAX_PW_LENGTH = 30;
 
@@ -16,6 +30,20 @@ bool isValidPW(pw) {
   return pw.length >= MIN_PW_LENGTH &&
       pw.length <= MAX_PW_LENGTH &&
       IDPW_REGEX.hasMatch(pw);
+}
+
+String? validatePW(pw) {
+  if (pw == null || pw.isEmpty) {
+    return '비밀번호를 입력해주세요.';
+  } else if (pw.length < MIN_PW_LENGTH) {
+    return '최소 $MIN_PW_LENGTH자 이상 입력해주세요.';
+  } else if (pw.length > MAX_PW_LENGTH) {
+    return '최대 $MAX_PW_LENGTH자 이하로 입력해주세요.';
+  } else if (!IDPW_REGEX.hasMatch(pw)) {
+    return '영문, 숫자, 특수문자(!@#\$&*~-)만 입력해주세요.';
+  } else {
+    return null;
+  }
 }
 
 bool isValidPWCheck(pw, pwCheck) {
