18b4b36fb69f7e37122113419950cb98983ff38c
parkjbdev <dev@parkjb.com>
2023-05-31 13:05:40 +0900

    fix: human error ;-)

--- lib/api/friends.dart
+++ lib/api/friends.dart
@@ -1,19 +1,17 @@
 import 'dart:convert';
+import 'package:cookie_app/utils/jwt.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart';
 
-Future<Map<String, dynamic>> apiGetFriends(
-    String? userid, String? phone) async {
-  final uri = Uri.https(dotenv.env['BASE_URI']!, '/friends', {
-    'userid': userid,
-    'phone': phone,
-  });
-
+Future<Map<String, dynamic>> apiGetFriends() async {
+  String token = (await JWT.read())!;
+  final uri = Uri.https(dotenv.env['BASE_URI']!, '/friends');
   try {
     Response res = await get(
       uri,
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
+        'authorization': 'Bearer $token'
       },
     );
     return json.decode(res.body);
