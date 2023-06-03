2d02628f6c3fafc13ceea88ed14ab9c951c7e68a
parkjbdev <dev@parkjb.com>
2023-06-04 02:54:40 +0900

    update: api with apimodels

--- lib/api/account/exists.dart
+++ lib/api/account/exists.dart
@@ -1,23 +1,25 @@
 import 'dart:convert';
+import 'package:cookie_app/model/api/account/exists.dart';
+import 'package:cookie_app/model/api/error.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart';
 
-Future<Map<String, dynamic>> apiGetExistance(
-    String? userid, String? phone) async {
+Future<ExistsResponse> apiGetExistance(String? userid, String? phone) async {
   final uri = Uri.https(dotenv.env['BASE_URI']!, '/account/exists', {
     'userid': userid,
     'phone': phone,
   });
-  try {
-    Response res = await get(
-      uri,
-      headers: <String, String>{
-        'Content-Type': 'application/json; charset=UTF-8',
-      },
-    );
-    return json.decode(res.body);
-  } catch (e) {
-    print('Error getting data from server: $e');
-    return {};
+
+  Response res = await get(
+    uri,
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8',
+    },
+  );
+
+  if (res.statusCode != 200) {
+    throw ErrorResponse.fromJson(json.decode(res.body));
   }
+
+  return ExistsResponse.fromJson(json.decode(res.body));
 }
--- lib/api/account/signup.dart
+++ lib/api/account/signup.dart
@@ -1,31 +1,30 @@
 import 'dart:convert';
-
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart';
 
-Future apiPostSignUp(data, profile) async {
+Future<bool> apiPostSignUp(data, profile) async {
   final uri = Uri.https(dotenv.env['BASE_URI']!, '/account/signup');
 
-  try {
-    MultipartRequest request = MultipartRequest('POST', uri);
+  MultipartRequest request = MultipartRequest('POST', uri);
 
-    request.fields['userid'] = data['userid'];
-    request.fields['password'] = data['password'];
-    request.fields['username'] = data['username'];
-    request.fields['birthday'] = data['birthday'];
-    request.fields['phone'] = data['phone'];
-    request.files.add(
-      await MultipartFile.fromPath(
-        'profile_image',
-        profile['image'].path,
-      ),
-    );
+  request.fields['userid'] = data['userid'];
+  request.fields['password'] = data['password'];
+  request.fields['username'] = data['username'];
+  request.fields['birthday'] = data['birthday'];
+  request.fields['phone'] = data['phone'];
+  request.files.add(
+    await MultipartFile.fromPath(
+      'profile_image',
+      profile['image'].path,
+    ),
+  );
 
-    StreamedResponse res = await request.send();
-    final body = await res.stream.bytesToString();
-    return json.decode(body);
-  } catch (e) {
-    print('Error sending data to server: $e');
-    return {'error': 'Error sending data to server'};
+  StreamedResponse res = await request.send();
+  final body = await res.stream.bytesToString();
+
+  if (res.statusCode != 201) {
+    throw json.decode(body);
   }
+
+  return true;
 }
