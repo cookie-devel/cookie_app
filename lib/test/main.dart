c3a3b1885077da62ee0d7792565071bf5928230a
parkjbdev <dev@parkjb.com>
2023-06-13 23:25:37 +0900

    Minor change: Implement `RoundedImage` via `BoxShape.circle` & Add: Test `main()` for testing UI

--- lib/test/main.dart
+++ lib/test/main.dart
@@ -1,3 +1,4 @@
+import 'package:cookie_app/view/components/RoundedImage.dart';
 import 'package:flutter/material.dart';
 import 'package:cookie_app/view/components/cookie.appbar.dart';
 
@@ -14,8 +15,13 @@ class TestApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       home: Scaffold(
         appBar: CookieAppBar(title: '테스트'),
-        body: const Center(),
+        body: const Center(
+          child: RoundedImage(
+            image: AssetImage('assets/images/kz1.png'),
+          ),
+        ),
       ),
     );
   }
 }
+// Run via `flutter run -t lib/test/main.dart`
