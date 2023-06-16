7206e8ae92561376f5946cb4f2a901b6bb3da29d
parkjbdev <dev@parkjb.com>
2023-06-17 04:33:08 +0900

    add viewmodel: room & message & minor change in account.viewmodel

--- lib/viewmodel/message.viewmodel.dart
+++ lib/viewmodel/message.viewmodel.dart
@@ -1,5 +1,15 @@
+import 'package:cookie_app/model/account/account_info.dart';
+import 'package:cookie_app/model/chat/message.dart';
+import 'package:cookie_app/viewmodel/account.viewmodel.dart';
 import 'package:cookie_app/viewmodel/base.viewmodel.dart';
 
 class MessageViewModel extends BaseViewModel {
-  MessageViewModel() : super();
+  late MessageModel _messageModel;
+  MessageViewModel({required MessageModel model}) : super() {
+    _messageModel = model;
+  }
+
+  PublicAccountViewModel get sender => PublicAccountViewModel(model: _messageModel.sender);
+  String get content => _messageModel.content;
+  DateTime get time => _messageModel.time;
 }
