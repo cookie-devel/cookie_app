62b4d06c00e1d2a5f923d7538482af8819733e2f
parkjbdev <dev@parkjb.com>
2023-07-21 19:22:06 +0900

    refactor: form

--- lib/view/components/auth/signin.form.dart
+++ lib/view/components/auth/signin.form.dart
@@ -1,20 +1,23 @@
-import 'package:cookie_app/view/components/CustomTextFormField.dart';
 import 'package:cookie_app/view/components/auth/submit_button.dart';
+import 'package:cookie_app/view/components/error_message.dart';
 import 'package:cookie_app/viewmodel/account.viewmodel.dart';
 import 'package:cookie_app/viewmodel/auth.viewmodel.dart';
 import 'package:flutter/material.dart';
-import 'package:cookie_app/view/components/auth/validator.dart';
 import 'package:provider/provider.dart';
+import 'package:cookie_app/view/components/text_form_fields.dart';
 
-// https://docs.flutter.dev/cookbook/forms/validation
+class SignInForm extends StatefulWidget {
+  const SignInForm({Key? key}) : super(key: key);
 
-class SignInForm extends StatelessWidget {
-  SignInForm({Key? key}) : super(key: key);
+  @override
+  State<SignInForm> createState() => _SignInFormState();
+}
 
+class _SignInFormState extends State<SignInForm> {
   final _formKey = GlobalKey<FormState>();
 
-  final IDField idField = IDField();
-  final PWField pwField = PWField();
+  String? id;
+  String? pw;
 
   @override
   Widget build(BuildContext context) {
@@ -25,77 +28,30 @@ class SignInForm extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            idField,
-            pwField,
-            signInButton(
-              context: context,
-              formKey: _formKey,
-              id: idField,
-              pw: pwField,
+            IDField(onSaved: (newValue) => id = newValue!),
+            PWField(onSaved: (newValue) => pw = newValue!),
+            SubmitButton(
+              onPressed: () async {
+                if (!_formKey.currentState!.validate()) return;
+                _formKey.currentState!.save();
+                return await Provider.of<AuthViewModel>(context, listen: false).signIn(
+                  id: id!,
+                  pw: pw!,
+                  privateAccountViewModel: Provider.of<PrivateAccountViewModel>(
+                    context,
+                    listen: false,
+                  ),
+                );
+              },
+              text: '로그인',
+              onFailure: (context, e) =>
+                  showErrorSnackBar(context, '로그인에 실패하였습니다. ${e.toString()}'),
             ),
           ].map(wrapped).toList(),
         ),
       ),
     );
   }
-}
-
-class IDField extends CustomTextFormField {
-  IDField({Key? key})
-      : super(
-          key: key,
-          labelText: "아이디",
-          validator: validateID,
-          autofillHints: [AutofillHints.username],
-        );
-}
-
-class PWField extends CustomTextFormField {
-  PWField({Key? key})
-      : super(
-          key: key,
-          obscureText: true,
-          labelText: "비밀번호",
-          validator: validatePW,
-          autofillHints: [AutofillHints.password],
-        );
-}
-
-SubmitButton signInButton({
-  required BuildContext context,
-  required GlobalKey<FormState> formKey,
-  required IDField id,
-  required PWField pw,
-}) {
-  return SubmitButton(
-    onPressed: () async {
-      if (!formKey.currentState!.validate()) return null;
-      formKey.currentState!.save();
-      await Provider.of<AuthViewModel>(context, listen: false).signIn(
-        id: id.value!,
-        pw: pw.value!,
-        privateAccountViewModel: Provider.of<PrivateAccountViewModel>(context, listen: false),
-      );
-      return null;
-    },
-    text: '로그인',
-    onSuccess: onSignInSuccess,
-    onFailure: onSignInFailure,
-  );
-}
-
-onSignInSuccess(context) {
-  const snackBar = SnackBar(
-    content: Text('Welcome!'),
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-}
-
-onSignInFailure(context) {
-  const snackBar = SnackBar(
-    content: Text('로그인에 실패하였습니다.'),
-  );
-  ScaffoldMessenger.of(context).showSnackBar(snackBar);
 }
 
 Container wrapped(child) {
--- lib/view/pages/signup.dart
+++ lib/view/pages/signup.dart
@@ -1,101 +1,19 @@
-import 'dart:io';
-import 'package:cookie_app/view/components/RoundedImage.dart';
 import 'package:cookie_app/view/components/auth/signup.form.dart';
 import 'package:cookie_app/view/components/cookie.appbar.dart';
 import 'package:flutter/material.dart';
-import 'package:cookie_app/view/components/ImageSelection.dart';
 
-class SignUpWidget extends StatefulWidget {
+class SignUpWidget extends StatelessWidget {
   const SignUpWidget({Key? key}) : super(key: key);
-
-  @override
-  State<SignUpWidget> createState() => _SignUpWidgetState();
-}
-
-class _SignUpWidgetState extends State<SignUpWidget> {
-  final _formKey = GlobalKey<FormState>();
-
-  IDField idField = IDField();
-  PWField pwField = PWField();
-  NameField nameField = NameField();
-  BirthdayField birthdayField = BirthdayField();
-  PhoneNumberField phoneNumberField = PhoneNumberField();
-
-  File? _imageFile;
-
-  @override
-  void initState() {
-    super.initState();
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: true,
       appBar: CookieAppBar(title: '회원가입'),
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.fromLTRB(10, 45, 10, 25),
-        child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              RoundedImage(
-                image: _imageFile == null
-                    ? const AssetImage("assets/images/kz1.png") as ImageProvider
-                    : FileImage(_imageFile!),
-              ),
-
-              // select image button
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                    side: const BorderSide(color: Colors.black45, width: 1),
-                    borderRadius: BorderRadius.circular(30),
-                  ),
-                ),
-                child: const Text('이미지 불러오기'),
-                onPressed: () async {
-                  final imageSelectionDialog = ImageSelectionDialog();
-                  final imageFile = await imageSelectionDialog.show(context);
-                  setState(() {
-                    if (imageFile != null) {
-                      _imageFile = File(imageFile.path);
-                    }
-                  });
-                },
-              ),
-              idField,
-              pwField,
-              PWCheckField(
-                pwField: pwField,
-              ),
-              nameField,
-              birthdayField,
-              phoneNumberField,
-              signUpButton(
-                context: context,
-                formKey: _formKey,
-                id: idField,
-                pw: pwField,
-                nameField: nameField,
-                birthdayField: birthdayField,
-                phoneNumberField: phoneNumberField,
-                profileImg: _imageFile,
-                profileMsg: null,
-              )
-            ].map(wrapped).toList(),
-          ),
-        ),
+      body: const SingleChildScrollView(
+        padding: EdgeInsets.fromLTRB(10, 45, 10, 25),
+        child: SignUpForm(),
       ),
     );
   }
-}
-
-Container wrapped(child) {
-  return Container(
-    margin: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-    child: child,
-  );
 }
