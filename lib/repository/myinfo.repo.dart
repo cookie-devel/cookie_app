707916a93783b5ba39484c968b8c79670693c9a7
parkjbdev <dev@parkjb.com>
2023-08-10 13:09:51 +0900

    cache: MyInfoRepositoryImpl

--- lib/repository/myinfo.repo.dart
+++ lib/repository/myinfo.repo.dart
@@ -2,20 +2,6 @@ import 'package:cookie_app/repository/api/account.dart';
 import 'package:cookie_app/repository/storage/account.storage.dart';
 import 'package:cookie_app/model/account/account_info.dart';
 
-// class MyInfoRepository {
-//   final MyInfoRepositoryPattern _repositoryStorage =
-//       MyInfoRepositoryStorageImpl();
-//   final MyInfoRepositoryPattern _repositoryAPI = MyInfoRepositoryApiImpl();
-
-//   MyInfoRepository();
-
-//   Future<PrivateAccountModel> getInfo() async {
-//     final _repository =
-//         await AccountStorage().isExist() ? _repositoryStorage : _repositoryAPI;
-//     return _repository.getInfo();
-//   }
-// }
-
 abstract class MyInfoRepository {
   Future<PrivateAccountModel> getInfo();
 }
@@ -31,5 +17,15 @@ class MyInfoRepositoryApiImpl implements MyInfoRepository {
   @override
   Future<PrivateAccountModel> getInfo() async {
     return (await AccountAPI.getInfo()).toPrivateAccount();
+  }
+}
+
+class MyInfoRepositoryImpl implements MyInfoRepository {
+  @override
+  Future<PrivateAccountModel> getInfo() async {
+    final _repository = await AccountStorage().isExist()
+        ? MyInfoRepositoryApiImpl()
+        : MyInfoRepositoryStorageImpl();
+    return _repository.getInfo();
   }
 }
