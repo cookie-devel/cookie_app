7432254a548777c36667901ef4a5806ad70c1369
parkjbdev <dev@parkjb.com>
2023-08-23 18:40:05 +0900

    chore: split datasource & repository

---
+++ lib/viewmodel/socket.viewmodel.dart
@@ -0,0 +1,26 @@
+import 'package:cookie_app/repository/myinfo.repo.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
+import 'package:socket_io_client/socket_io_client.dart';
+
+class ChatViewModel {
+  const ChatViewModel();
+  static final log = Logger('ChatViewModel');
+  static final Socket socket = io(
+    OptionBuilder()
+        .setTransports(['websocket'])
+        .disableAutoConnect()
+        .enableReconnection()
+        .build(),
+  );
+
+  void connect() {
+      log.info("socket connected; id: ${socket.id}");
+      socket.connect();
+  }
+
+  void disconnect() {
+    socket.disconnect();
+  }
+}
