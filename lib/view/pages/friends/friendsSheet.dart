cce34adab621e8035a7901d1ff7956850f45cf4c
parkjbdev <dev@parkjb.com>
2023-09-07 11:04:43 +0900

    minor change: friendsActionSheet

--- lib/view/pages/friends/friendsSheet.dart
+++ lib/view/pages/friends/friendsSheet.dart
@@ -40,78 +40,57 @@ class FriendsAction extends StatelessWidget {
     });
   }
 
-  List<ListTile> listTileItems(context) {
-    return [
-      ListTile(
-        title: const Text('친구관리'),
-        leading: const Icon(Icons.person),
-        onTap: () {},
-      ),
-      ListTile(
-        title: const Text(
-          '알림',
-        ),
-        leading: const Icon(Icons.notifications),
-        onTap: () {},
-      ),
-      ListTile(
-        title: const Text(
-          '정렬',
-        ),
-        leading: const Icon(Icons.sort),
-        onTap: () {
-          _sortFriendsList(context);
-        },
-      ),
-      ListTile(
-        title: const Text('개인정보'),
-        leading: const Icon(Icons.privacy_tip),
-        onTap: () {},
-      ),
-      ListTile(
-        title: const Text('도움말'),
-        leading: const Icon(Icons.help),
-        onTap: () {},
-      ),
-    ];
-  }
-
   @override
   Widget build(BuildContext context) {
     return IconButton(
       icon: const Icon(Icons.people),
-      onPressed: () {
-        showModalBottomSheet<void>(
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
-          ),
-          context: context,
-          builder: (BuildContext context) {
-            return Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                IconButton(
-                  icon: const Icon(
-                    Icons.keyboard_arrow_down,
-                    color: Colors.black45,
-                    size: 24,
+      onPressed: () => showModalBottomSheet<void>(
+        context: context,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+        ),
+        builder: (BuildContext context) => Column(
+          children: [
+            const SizedBox(
+              height: 8,
+            ),
+            Expanded(
+              child: ListView(
+                shrinkWrap: true,
+                children: [
+                  ListTile(
+                    title: const Text('친구관리'),
+                    leading: const Icon(Icons.person),
+                    onTap: () {},
                   ),
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                ),
-                const Divider(height: 2),
-                Expanded(
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: listTileItems(context),
+                  ListTile(
+                    title: const Text('알림'),
+                    leading: const Icon(Icons.notifications),
+                    onTap: () {},
                   ),
-                ),
-              ],
-            );
-          },
-        );
-      },
+                  ListTile(
+                    title: const Text('정렬'),
+                    leading: const Icon(Icons.sort),
+                    onTap: () {
+                      _sortFriendsList(context);
+                    },
+                  ),
+                  ListTile(
+                    title: const Text('개인정보'),
+                    leading: const Icon(Icons.privacy_tip),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    title: const Text('도움말'),
+                    leading: const Icon(Icons.help),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
