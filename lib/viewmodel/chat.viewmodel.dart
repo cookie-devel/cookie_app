1d419b86ea6c21ddfbee8cc8639be71ec1870860
parkjbdev <dev@parkjb.com>
2023-09-07 11:07:43 +0900

    new: chatroom ui & viewmodel

--- lib/viewmodel/chat.viewmodel.dart
+++ lib/viewmodel/chat.viewmodel.dart
@@ -1,8 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:cookie_app/viewmodel/chat/room.viewmodel.dart';
 import 'package:logging/logging.dart';
 import 'package:socket_io_client/socket_io_client.dart';
 
-class ChatViewModel {
-  const ChatViewModel();
+class ChatViewModel extends ChangeNotifier {
   static final log = Logger('ChatViewModel');
   static final Socket socket = io(
     OptionBuilder()
@@ -19,5 +20,29 @@ class ChatViewModel {
 
   void disconnect() {
     socket.disconnect();
+  }
+
+  ChatViewModel();
+  List<ChatRoomViewModel> _roomViewModel = [];
+  List<ChatRoomViewModel> get roomViewModel => _roomViewModel;
+
+  void addRoom(ChatRoomViewModel room) {
+    // TODO: add room to server
+    _roomViewModel.add(room);
+  }
+
+  void removeRoom(ChatRoomViewModel room) {
+    // TODO: remove room to server
+    _roomViewModel.remove(room);
+  }
+
+  void updateRoom(ChatRoomViewModel room) {
+    // TODO: update room from server
+    _roomViewModel[_roomViewModel.indexOf(room)] = room;
+  }
+
+  void updateRooms(List<ChatRoomViewModel> rooms) {
+    // TODO: update rooms from server
+    _roomViewModel = rooms;
   }
 }
