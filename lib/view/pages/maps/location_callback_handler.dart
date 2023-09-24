48d49b73d7655c9490d5760cee230892cb59caae
elic121 <elic121@ajou.ac.kr>
2023-09-24 16:16:50 +0900

    [add] background locator

---
+++ lib/view/pages/maps/location_callback_handler.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:background_locator_2/location_dto.dart';
+import 'package:cookie_app/repository/location_service_repo.dart';
+
+@pragma('vm:entry-point')
+class LocationCallbackHandler {
+  @pragma('vm:entry-point')
+  static Future<void> initCallback(Map<dynamic, dynamic> params) async {
+    LocationServiceRepository myLocationCallbackRepository =
+        LocationServiceRepository();
+    await myLocationCallbackRepository.init(params);
+  }
+
+  @pragma('vm:entry-point')
+  static Future<void> disposeCallback() async {
+    LocationServiceRepository myLocationCallbackRepository =
+        LocationServiceRepository();
+    await myLocationCallbackRepository.dispose();
+  }
+
+  @pragma('vm:entry-point')
+  static Future<void> callback(LocationDto locationDto) async {
+    LocationServiceRepository myLocationCallbackRepository =
+        LocationServiceRepository();
+    await myLocationCallbackRepository.callback(locationDto);
+  }
+
+  @pragma('vm:entry-point')
+  static Future<void> notificationCallback() async {
+    print('***notificationCallback');
+  }
+}
--- lib/viewmodel/map.viewmodel.dart
+++ lib/viewmodel/map.viewmodel.dart
@@ -2,12 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:logging/logging.dart';
 import 'package:cookie_app/types/map/mapPosition_info.dart';
-import 'package:permission_handler/permission_handler.dart';
-import 'package:geolocator/geolocator.dart';
+import 'dart:async';
 
 class MapProvider with ChangeNotifier {
   Logger logger = Logger('MapProvider');
-  late LatLng _currentLocation;
+  LatLng _currentLocation = const LatLng(0, 0);
   late List<MapPosition> _mapLog = [];
   bool _loading = true;
 
@@ -16,13 +15,11 @@ class MapProvider with ChangeNotifier {
   bool get loading => _loading;
 
   MapProvider() {
-    _locationPermission();
-    _getUserLocation();
     _getMapData();
   }
 
-  void setCurrentLocation(LatLng location) {
-    _currentLocation = location;
+  void setCurrentLocation(double latitude, double longitude) {
+    _currentLocation = LatLng(latitude, longitude);
     _loading = false;
     notifyListeners();
     logger.info("mapProvider = $_currentLocation");
@@ -33,56 +30,5 @@ class MapProvider with ChangeNotifier {
     _mapLog = [];
     notifyListeners();
     logger.info("mapLog = $_mapLog");
-  }
-
-  Future<void> _locationPermission() async {
-    final PermissionStatus requestStatus = await Permission.location.request();
-    final PermissionStatus status = await Permission.location.status;
-
-    if (requestStatus.isGranted && status.isLimited) {
-      await _handleLimitedPermission();
-    } else if (requestStatus.isPermanentlyDenied ||
-        status.isPermanentlyDenied) {
-      _requestPermanentPermission();
-    } else if (status.isRestricted) {
-      _requestRestrictedPermission();
-    }
-  }
-
-  Future<void> _handleLimitedPermission() async {
-    final bool isLocationServiceEnabled =
-        await Permission.locationWhenInUse.serviceStatus.isEnabled;
-
-    if (isLocationServiceEnabled) {
-      _getUserLocation();
-    } else {
-      // TODO: 위치 서비스가 꺼져 있는 경우 처리할 내용 추가
-    }
-  }
-
-  void _requestPermanentPermission() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      openAppSettings();
-    });
-  }
-
-  void _requestRestrictedPermission() {
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      openAppSettings();
-    });
-  }
-
-  Future<void> _getUserLocation() async {
-    try {
-      Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.high,
-      );
-      _currentLocation = LatLng(position.latitude, position.longitude);
-      setCurrentLocation(_currentLocation);
-      notifyListeners();
-      logger.info("currentLocation = $_currentLocation");
-    } catch (e) {
-      logger.warning("Failed to get user location: $e");
-    }
   }
 }
