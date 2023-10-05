c4da01cfd68c887602af62f12aa378a65a7b62ad
elic121 <elic121@ajou.ac.kr>
2023-10-05 16:17:38 +0900

    [add] location model

---
+++ lib/model/map/location_info.dart
@@ -0,0 +1,21 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'location_info.g.dart';
+
+@JsonSerializable()
+class LocationModel{
+  String id;
+  double latitude;
+  double longitude;
+
+  LocationModel({
+    required this.id,
+    required this.latitude,
+    required this.longitude,
+  });
+
+  factory LocationModel.fromJson(Map<String, dynamic> json) => _$LocationModelFromJson(json);
+
+  Map<String, dynamic> toJson() => _$LocationModelToJson(this);
+  
+}
