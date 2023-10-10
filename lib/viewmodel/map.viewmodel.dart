eba23df2ce8f57a203a9b29fc9f8d0d4e9e094b3
elic121 <elic121@ajou.ac.kr>
2023-10-10 12:05:36 +0900

    <update> map function

--- lib/viewmodel/map.viewmodel.dart
+++ lib/viewmodel/map.viewmodel.dart
@@ -1,24 +1,59 @@
-import 'dart:async';
 
+import 'package:cookie_app/viewmodel/friends.viewmodel.dart';
+import 'package:cookie_app/types/map/mapPosition_info.dart';
+import 'package:cookie_app/model/account/account_info.dart';
+import 'package:cookie_app/view/navigation_service.dart';
+import 'package:cookie_app/repository/jwt.repo.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:logging/logging.dart';
+import 'package:socket_io_client/socket_io_client.dart';
 
-import 'package:cookie_app/types/map/mapPosition_info.dart';
+class MapEvents {
+  static const getPosition = 'get_position';
+  static const sendPosition = 'send_position';
+}
 
 class MapProvider with ChangeNotifier {
   Logger logger = Logger('MapProvider');
+
+  final Socket socket = io(
+    Uri(
+      scheme: dotenv.env['API_SCHEME'],
+      host: dotenv.env['API_HOST'],
+      port: int.parse(dotenv.env['API_PORT']!),
+      path: '/map',
+    ).toString(),
+    OptionBuilder()
+        .setTransports(['websocket'])
+        .disableAutoConnect()
+        .enableReconnection()
+        .build(),
+  );
+
+  bool _connected = false;
+  bool get connected => _connected;
+
+  Function get connect => socket.connect;
+  Function get disconnect => socket.disconnect;
+
+  final List<MarkerInfo> _mapLog = [];
   LatLng _currentLocation = const LatLng(0, 0);
-  late List<MapPosition> _mapLog = [];
   bool _loading = true;
 
+  List<MarkerInfo> get mapLog => _mapLog;
   LatLng get currentLocation => _currentLocation;
-  List get mapLog => _mapLog;
   bool get loading => _loading;
 
   MapProvider() {
-    _getMapData();
+    socket.auth = {'token': JWTRepository.token!};
+
+    socket.onConnect(_onConnectionChange);
+    socket.onDisconnect(_onConnectionChange);
+    socket.on(MapEvents.getPosition, _onGetPosition);
+    socket.on(MapEvents.sendPosition, _onSendPosition);
   }
 
   void setCurrentLocation(double latitude, double longitude) {
@@ -28,10 +63,40 @@ class MapProvider with ChangeNotifier {
     logger.info("mapProvider = $_currentLocation");
   }
 
-  Future<void> _getMapData() async {
-    //TODO: get data from server
-    _mapLog = [];
+  // Socket Incoming Event Handlers
+  void _onConnectionChange(_) {
+    _connected = socket.connected;
+    socket.connected
+        ? logger.info('socket connected')
+        : logger.warning('socket not connected');
     notifyListeners();
-    logger.info("mapLog = $_mapLog");
+  }
+
+  void _onSendPosition(_) {
+    logger.info("send_position");
+    socket.emit(MapEvents.sendPosition, {
+      'latitude': _currentLocation.latitude,
+      'longitude': _currentLocation.longitude,
+    });
+  }
+
+  BuildContext context = NavigationService.navigatorKey.currentContext!;
+  void _onGetPosition(data) {
+    logger.info("get_position: $data");
+
+    final List<MapInfoResponse> info =
+        data.map((e) => MapInfoResponse.fromJson(e)).toList();
+
+    final List<MarkerInfo> _mapLog = info
+        .map(
+          (e) => MarkerInfo(
+            info: Provider.of<FriendsViewModel>(context, listen: false)
+                .getFriend(e.userid) as PublicAccountModel,
+            latitude: e.latitude,
+            longitude: e.longitude,
+          ),
+        )
+        .toList();
+    notifyListeners();
   }
 }
