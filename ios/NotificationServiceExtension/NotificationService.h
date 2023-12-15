398cd6742091dca086d0344420b87a99149e4ff0
Park JongBeum <dev@parkjb.com>
2023-12-16 03:20:02 +0900

    ios_feat: add image notification support

---
+++ ios/NotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotificationServiceExtension
+//
+//  Created by 박종범 on 12/16/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
